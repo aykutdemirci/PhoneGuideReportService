b715712385c746ae52d7df09b9f8749c837eb1fe
Aykut DEMİRCİ <aykutdemirci@outlook.com>
2024-06-11 16:36:30 +0300

    unit of work added

---
+++ Core/PhoneGuideReportService.Application/Abstractions/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using PhoneGuideReportService.Application.Repositories;
+
+namespace PhoneGuideReportService.Application.Abstractions.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        public IReportRepository ReportRepository { get; }
+
+        Task SaveAsync();
+    }
+}
---
+++ Infrastructure/PhoneGuideReportService.Persistance/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using PhoneGuideReportService.Application.Abstractions.UnitOfWork;
+using PhoneGuideReportService.Application.Repositories;
+using PhoneGuideReportService.Persistance.Context;
+
+namespace PhoneGuideReportService.Persistance
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public IReportRepository ReportRepository { get; }
+
+        private readonly PhoneGuideReportDbContext _dbContext;
+
+        public UnitOfWork(IReportRepository reportRepository, PhoneGuideReportDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            ReportRepository = reportRepository;
+        }
+
+        public async Task SaveAsync()
+        {
+            using var transaction = await _dbContext.Database.BeginTransactionAsync();
+
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+                transaction.Commit();
+            }
+            catch
+            {
+                transaction.Rollback();
+                throw;
+            }
+        }
+    }
+}
