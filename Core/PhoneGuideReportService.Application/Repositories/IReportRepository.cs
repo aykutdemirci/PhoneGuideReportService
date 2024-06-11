58dd2c3f9299dfa82806d2f2da26241907fdcc29
Aykut DEMİRCİ <aykutdemirci@outlook.com>
2024-06-11 16:25:26 +0300

    custom repository

---
+++ Core/PhoneGuideReportService.Application/Repositories/IReportRepository.cs
@@ -0,0 +1,8 @@
+﻿using PhoneGuideReportService.Domain.Entities;
+
+namespace PhoneGuideReportService.Application.Repositories
+{
+    public interface IReportRepository : IRepository<Report>
+    {
+    }
+}
---
+++ Infrastructure/PhoneGuideReportService.Persistance/Repositories/ReportRepository.cs
@@ -0,0 +1,13 @@
+﻿using PhoneGuideReportService.Application.Repositories;
+using PhoneGuideReportService.Domain.Entities;
+using PhoneGuideReportService.Persistance.Context;
+
+namespace PhoneGuideReportService.Persistance.Repositories
+{
+    public class ReportRepository : Repository<Report>, IReportRepository
+    {
+        public ReportRepository(PhoneGuideReportDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
