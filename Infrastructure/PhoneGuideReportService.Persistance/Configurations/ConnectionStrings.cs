768328d2992e8460906cbeaaaec22dac298c1a0a
Aykut DEMİRCİ <aykutdemirci@outlook.com>
2024-06-11 16:21:37 +0300

    dbcontext added

---
+++ Infrastructure/PhoneGuideReportService.Persistance/Configurations/ConnectionStrings.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace PhoneGuideReportService.Persistance.Configurations
+{
+    static class ConnectionStrings
+    {
+        public static string PostgreSQL
+        {
+            get
+            {
+                var cfgManager = new ConfigurationManager();
+                cfgManager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../../Presentation/PhoneGuideReportService.API"));
+                cfgManager.AddJsonFile("appsettings.json");
+                return cfgManager.GetConnectionString("PostgreSQL");
+            }
+        }
+    }
+}
---
+++ Infrastructure/PhoneGuideReportService.Persistance/ServiceRegistrations.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PhoneGuideReportService.Persistance.Configurations;
+using PhoneGuideReportService.Persistance.Context;
+
+namespace PhoneGuideReportService.Persistance
+{
+    public static class ServiceRegistrations
+    {
+        public static void AddPersistanceService(this IServiceCollection services)
+        {
+            services.AddDbContext<PhoneGuideReportDbContext>(opts => opts.UseNpgsql(ConnectionStrings.PostgreSQL));
+        }
+    }
+}
