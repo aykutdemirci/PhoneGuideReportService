6b85a942cf1c469ffe361d352ef5d08ea8af65eb
Aykut DEMİRCİ <aykutdemirci@outlook.com>
2024-06-12 05:02:09 +0300

    queue consumer

--- QueueConsumer/QueueConsumer/Program.cs
+++ QueueConsumer/QueueConsumer/Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "report_queue",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+};
+
+channel.BasicConsume(queue: "report_queue",
+                     autoAck: true,
+                     consumer: consumer);
+
+Console.ReadLine();
