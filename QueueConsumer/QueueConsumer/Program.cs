d3a4ecc4a63f9e56b6faa9747fa06ca3ba6f7583
Aykut DEMİRCİ <aykutdemirci@outlook.com>
2024-06-12 05:10:12 +0300

    yorum satırları

--- QueueConsumer/QueueConsumer/Program.cs
+++ QueueConsumer/QueueConsumer/Program.cs
@@ -12,15 +12,18 @@ channel.QueueDeclare(queue: "report_queue",
                      autoDelete: false,
                      arguments: null);
 
-Console.WriteLine(" [*] Waiting for messages.");
-
 var consumer = new EventingBasicConsumer(channel);
 
 consumer.Received += (model, ea) =>
 {
     var body = ea.Body.ToArray();
     var message = Encoding.UTF8.GetString(body);
-    Console.WriteLine($" [x] Received {message}");
+    Console.WriteLine($"Received {message}");
+
+    //Bu aşamada raporda istenen verileri almak için bir http istek ile diğer servise gidilecek.
+    //Rapor bilgileri hazırlandıktan sonra bir excel dosyaya yazılarak dosyanın url'si, PhoneGuideReportDb'deki Reports tablosuna field olarak eklenip bu field'a yazılacak
+    //Raporun durumu "tamamlandı" olarak güncellenecek
+    //Kullanıcı rapor raporu talep ettiğinde dosyanın url'si gönderilecek kullanıcının dosyayı indirmesi sağlanacak
 };
 
 channel.BasicConsume(queue: "report_queue",
